b61d8423265b2553be916b75f872e2434e8e0e7a
Legend108 <80154701+Legend108@users.noreply.github.com>
2022-06-17 22:49:58 +0530

    Add logging option

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const configF = require("./utils/config");
   const config = await configF();
 
   const discordBot = new bot({
-    token: config.token,
-    onReadyMessage: "{username} has logged in",
+    token: config.token, // Bot token for logging in.
+    onReadyMessage: "{username} has logged in", // On ready message, {username} get's replaced with bot's real name.
+    logging: true, // Default is true, set it to false to stop logging
   });
 
   discordBot.start();
