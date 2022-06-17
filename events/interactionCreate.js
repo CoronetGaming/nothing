8e8dd16a8efaa486e5ad8c0763f9f04421127560
Legend108 <80154701+Legend108@users.noreply.github.com>
2022-06-17 22:19:02 +0530

    Add files via upload

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,1 +1,21 @@
+module.exports = {
+  name: "interactionCreate",
+  async run(client, interaction) {
+    if (interaction.isCommand()) {
+      const command = client.commands.get(interaction.commandName);
 
+      if (command) {
+        try {
+          command.exec(client, interaction);
+        } catch (error) {
+          return interaction.reply({
+            content: "An error occured, while executing",
+            epheremal: true,
+          });
+        }
+      } else {
+        return;
+      }
+    }
+  },
+};
